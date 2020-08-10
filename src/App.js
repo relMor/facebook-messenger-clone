01ca6e8ec452bcc452950e8e2ef2581c712a4dfc
relMor <amanvarma.600847@gmail.com>
2020-08-10 16:26:59 -0700

    created front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([{ username: "Aman", text: "NOICE!!" }]);
+  const [username, setUsername] = useState("");
+
+  // console.log(input);
+  // console.log(messages);
+
+  useEffect(() => {
+    setUsername(prompt("Please enter your name..."));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { username: username, text: input }]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Facebook Messenger Clone</h1>
+      <h3>Welcome {username}</h3>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+
+      {messages.map((message) => (
+        <Message message={message} username={username} key={Math.random()}/>
+      ))}
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./Message.css";
+
+function Message({ username, message }) {
+  const isUser = username === message.username;
+
+  return (
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography color="textSecondary" variant="h5" component="h2">
+            {message.username}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default Message;
